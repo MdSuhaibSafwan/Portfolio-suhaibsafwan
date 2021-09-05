aa7d2ae2dbfec95b586230f0f9ff6b42976da45d
MD Suhaib Safwan <suhaibsafwan45@gmail.com>
2021-09-05 15:43:44 +0600

    added gitignore

--- main/models.py
+++ main/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# class UserImages(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="profile_images")
+#     image = models.ImageField(upload_to="profile/user/picture")
+
+#     def __str__(self):
+#         return self.user.username + " image"
+    
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # image = models.ImageField(upload_to="profile/user/picture", null=True)
+    github_url = models.URLField()
+    facebook_url = models.URLField()
+    upwork_url = models.URLField()
+    fiverr_url = models.URLField()
+
+
+class WorkExperience(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="experiences")
+    description = models.TextField()
